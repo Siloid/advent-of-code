d715941168c2893f78caa25f85f1948f9c3c7211
Siloid <siloid@gmail.com>
2024-12-01 14:44:01 -0500

    Fix whitespace

--- 2024/Day01/main.go
+++ 2024/Day01/main.go
@@ -10,65 +10,65 @@ import (
 )
 
 func parseInput(path string) ([]int, []int, map[int]int) {
-	var leftLocationIDs []int
-	var rightLocationIDs []int
-	similarityMap := make(map[int]int)
+    var leftLocationIDs []int
+    var rightLocationIDs []int
+    similarityMap := make(map[int]int)
 
-	file, _ := os.Open(path)
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		currentLine := scanner.Text()
-		locations := strings.Fields((currentLine))
+    file, _ := os.Open(path)
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        currentLine := scanner.Text()
+        locations := strings.Fields((currentLine))
 
-		leftLocation, _ := strconv.Atoi(locations[0])
-		leftLocationIDs = append(leftLocationIDs, leftLocation)
+        leftLocation, _ := strconv.Atoi(locations[0])
+        leftLocationIDs = append(leftLocationIDs, leftLocation)
 
-		rightLocation, _ := strconv.Atoi(locations[1])
-		rightLocationIDs = append(rightLocationIDs, rightLocation)
+        rightLocation, _ := strconv.Atoi(locations[1])
+        rightLocationIDs = append(rightLocationIDs, rightLocation)
 
-		count, inMap := similarityMap[rightLocation]
-		if inMap {
-			similarityMap[rightLocation] = count + 1
-		} else {
-			similarityMap[rightLocation] = 1
-		}
-	}
+        count, inMap := similarityMap[rightLocation]
+        if inMap {
+            similarityMap[rightLocation] = count + 1
+        } else {
+            similarityMap[rightLocation] = 1
+        }
+    }
 
-	sort.Ints(leftLocationIDs)
-	sort.Ints(rightLocationIDs)
+    sort.Ints(leftLocationIDs)
+    sort.Ints(rightLocationIDs)
 
-	return leftLocationIDs, rightLocationIDs, similarityMap
+    return leftLocationIDs, rightLocationIDs, similarityMap
 }
 
 func absDiff(a int, b int) int {
-	if a < b {
-		return b - a
-	}
-	return a - b
+    if a < b {
+        return b - a
+    }
+    return a - b
 }
 
 func similarity(id int, similarityMap map[int]int) int {
-	similityMultiplier, inMap := similarityMap[id]
-	if inMap {
-		return similityMultiplier * id
-	} else {
-		return 0
-	}
+    similityMultiplier, inMap := similarityMap[id]
+    if inMap {
+        return similityMultiplier * id
+    } else {
+        return 0
+    }
 }
 
 func main() {
-	leftIDs, rightIDs, similarityMap := parseInput("./input.txt")
+    leftIDs, rightIDs, similarityMap := parseInput("./input.txt")
 
-	var totalDifference int = 0
-	var totalSimilarity int = 0
-	for index, id := range leftIDs {
-		// Part 1
-		totalDifference += absDiff(id, rightIDs[index])
+    var totalDifference int = 0
+    var totalSimilarity int = 0
+    for index, id := range leftIDs {
+        // Part 1
+        totalDifference += absDiff(id, rightIDs[index])
 
-		// Part 2
-		totalSimilarity += similarity(id, similarityMap)
-	}
+        // Part 2
+        totalSimilarity += similarity(id, similarityMap)
+    }
 
-	fmt.Printf("(Part 1) - Total Difference: %d\n", totalDifference)
-	fmt.Printf("(Part 2) - Total Similarity: %d\n", totalSimilarity)
+    fmt.Printf("(Part 1) - Total Difference: %d\n", totalDifference)
+    fmt.Printf("(Part 2) - Total Similarity: %d\n", totalSimilarity)
 }
