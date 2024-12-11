cf3fc5d9693e91503743b18b183c6f11d7d10579
Siloid <siloid@gmail.com>
2024-12-11 12:54:25 -0500

    Add Day11 Part 2 solution

--- 2024/Day11/main.go
+++ 2024/Day11/main.go
@@ -6,11 +6,13 @@ import (
 	"slices"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 
 type stone struct {
-    value int
+    value int // value of stone
+    count int // number of stones with same value
 }
 
 func (s *stone) blink() int { 
@@ -45,34 +47,76 @@ func parseInput(path string) []int {
     return stoneValues
 }
 
-func blinkNTimes(stoneLine []stone, timesToBlink int) []stone {
+func blinkNTimesAndCount(stoneLine []stone, timesToBlink int) int { //[]stone {
     for range timesToBlink {
         // work from the right and append to the right to ensure correct ordering
         for i := len(stoneLine) -1 ; i >= 0 ; i-- {
             newStoneValue := stoneLine[i].blink()
             if newStoneValue != -1 {
-                newStone := stone{newStoneValue}
+                newStone := stone{newStoneValue, stoneLine[i].count}
                 stoneLine = slices.Insert(stoneLine, i + 1, newStone)
             }
         }
+        // reduce the stones
+        for i := len(stoneLine) -1 ; i >= 0 ; i-- {
+            for j, _ := range stoneLine {
+                if j >= i {break}
+                // if two stones have the same value, sum the counts and remove 1 stone
+                if stoneLine[i].value == stoneLine[j].value {
+                    stoneLine[j].count += stoneLine[i].count
+                    stoneLine = slices.Delete(stoneLine, i, i+1)
+                    break
+                }
+            }
+        }
     }
-    return stoneLine
+    totalStones := 0
+    for _, aStone := range stoneLine {
+        totalStones += aStone.count
+    }
+    return totalStones
 }
 
-func createStones(values []int) []stone {
-    var stoneLine []stone
+func createStoneLines(values []int) [][]stone {
+    // each stone get it's own line as they don't interfere with one another
+    // this way we can more easily parallelize in the next step
+    var stoneLines [][]stone
     for _, value := range values {
-        newStone := stone{value}
-        stoneLine = append(stoneLine, newStone)
+        newStoneLine := []stone{stone{value, 1}}
+        stoneLines = append(stoneLines, newStoneLine)
     }
-    return stoneLine
+    return stoneLines
+}
+
+func runIt(stoneLines [][]stone, timesToBlink int) int {
+    var wg sync.WaitGroup
+    results := make([]int, len(stoneLines))
+    for index, stoneLine := range stoneLines {
+        wg.Add(1)
+        go func(i int, sl []stone) {
+            defer wg.Done()
+            results[i] = blinkNTimesAndCount(sl, timesToBlink)
+        }(index, stoneLine)
+    }
+    wg.Wait()
+
+    totalStones := 0
+    for _, stoneCount := range results {
+        totalStones += stoneCount
+    }
+    return totalStones
 }
 
 func main() {
     stoneValues := parseInput("input.txt")
-    stoneLine := createStones(stoneValues)
-    
+
     // Part1
-    stoneLine = blinkNTimes(stoneLine, 25)
-    fmt.Printf("(Part 1) - Stone total after 25 blinks: %d\n", len(stoneLine))
+    stoneLines := createStoneLines(stoneValues)
+    totalStones := runIt(stoneLines, 25)
+    fmt.Printf("(Part 1) - Stone total after 25 blinks: %d\n", totalStones)
+
+    // Part2
+    stoneLines = createStoneLines(stoneValues)
+    totalStones = runIt(stoneLines, 75)
+    fmt.Printf("(Part 2) - Stone total after 75 blinks: %d\n", totalStones)
 }
